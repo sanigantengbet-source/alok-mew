1ed30725f75ecac2c95b037909f1cfd9a9158e95
ALOK GANTENG <sanigantengbet@gmail.com>
2026-08-26 23:35:42 +0700

    Upload components/Navbar/BottomNav.tsx

--- components/Navbar/BottomNav.tsx
+++ components/Navbar/BottomNav.tsx
@@ -4,26 +4,7 @@ import React from 'react';
 import { Home, Flame, Tv, Settings } from 'lucide-react';
 import { useApp } from '@/context/AppContext';
 import { PageView } from '@/types';
-
-// Distinctive YouTube Shorts custom SVG icon
-const ShortsIcon: React.FC<{ className?: string; isActive?: boolean }> = ({
-  className = 'w-5 h-5',
-  isActive = false,
-}) => (
-  <svg
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className={className}
-    aria-hidden="true"
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M17.77 4.96c-1.39-1.39-3.65-1.39-5.04 0L6.46 11.23c-1.74 1.74-1.74 4.56 0 6.31 1.74 1.74 4.56 1.74 6.31 0l6.27-6.27c1.74-1.74 1.74-4.57 0-6.31zm-1.06 1.06c1.16 1.16 1.16 3.03 0 4.19l-6.27 6.27c-1.16 1.16-3.03 1.16-4.19 0-1.16-1.16-1.16-3.03 0-4.19l6.27-6.27c1.16-1.16 3.03-1.16 4.19 0z"
-    />
-    <polygon points="10,8.5 16,12 10,15.5" fill={isActive ? '#dc2626' : 'currentColor'} />
-  </svg>
-);
+import { ShortsIcon } from '@/components/Icons/ShortsIcon';
 
 export const BottomNav: React.FC = () => {
   const {
